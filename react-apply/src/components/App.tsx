ea3d861f96c9d0684d4bf4299a9200de477820fe
cookie.729 <cookie.729@icloud.com>
2022-08-14 22:59:19 +0900

    簡易メモ実装

--- react-apply/src/components/App.tsx
+++ react-apply/src/components/App.tsx
@@ -1,42 +1,43 @@
-import { ChangeEvent, useState, FC } from "react";
+import {ChangeEvent, useState,FC } from "react";
 import styled from "styled-components";
-import { MemoList } from "./MemoList";
 
 export const App: FC = () => {
-  // テキストボックス State
   const [text, setText] = useState<string>("");
-  // メモ一覧 State
   const [memos, setMemos] = useState<string[]>([]);
+  const onChangeText = (e: ChangeEvent<HTMLInputElement>) => setText(e.target.value);
 
-  // テキストボックス入力時に入力内容をStateに設定
-  const onChangeText = ( e: ChangeEvent<HTMLInputElement>) => setText(e.target.value);
-
-  // [追加]ボタン押下時
   const onClickAdd = () => {
-    // State変更を正常に検知させるため新たな配列を生成
     const newMemos = [...memos];
-    // テキストボックスの入力内容をメモ配列に追加
     newMemos.push(text);
     setMemos(newMemos);
-    // テキストボックスを空に
     setText("");
   };
 
-  // [削除]ボタン押下時（何番目が押されたかを引数で受け取る）
-  const onClickDelete = useCallback((index: number) => {
-    // State変更を正常に検知させるため新たな配列を生成
+  const onClickDelete = (index: number) => {
     const newMemos = [...memos];
-    // メモ配列から該当の要素を削除
     newMemos.splice(index, 1);
     setMemos(newMemos);
-  }, [memos]);
+  };
 
-  return (
+
+  return ( 
     <div>
       <h1>簡易メモ</h1>
       <input type="text" value={text} onChange={onChangeText} />
       <SButton onClick={onClickAdd}>追加</SButton>
-      <MemoList memos={memos} onClickDelete={onClickDelete} />
+      <SContainer>
+        <p>メモ一覧</p>
+        <ul>
+          {memos.map((memo, index) => (
+            <li key={memo}>
+              <SMemoWrapper>
+                <p>{memo}</p>
+                <SButton onClick={() => onClickDelete(index)}>削除</SButton>
+              </SMemoWrapper>
+            </li>
+          ))}
+        </ul>
+      </SContainer>
     </div>
   );
 };
@@ -44,4 +45,12 @@ export const App: FC = () => {
 const SButton = styled.button`
   margin-left: 16px;
   `;
-  
+const SContainer = styled.div`
+  border: solid 1px #ccc;
+  padding: 16px;
+  margin: 8px;
+  `;
+const SMemoWrapper = styled.div`
+  display: flex;
+  align-items: center;
+`;
--- react-apply/src/index.tsx
+++ react-apply/src/index.tsx
@@ -1,9 +1,6 @@
 import { render } from "react-dom";
 import { App } from "./components/App";
 
+
 const rootElement = document.getElementById("root");
 render(<App />, rootElement);
-export const App = () => {
-  return <h1>簡易メモ</h1>;
-};
-
