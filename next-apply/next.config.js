7736763f575cd3cd0557df246b54f4d589c6a549
cookie.729 <cookie.729@icloud.com>
2022-08-18 04:24:53 +0900

    nextでのアプリ作成

---
+++ next-apply/next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  compiler: {
+    // styledComponents有効化
+    styledComponents: true,
+  },
+}
+
+module.exports = nextConfig
